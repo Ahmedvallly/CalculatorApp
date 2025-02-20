95e481065def6cd46f0758cfee7b96cc2a3ccc65
lab_services_student <st10251131@vcconnect.edu.za>
2025-02-20 10:01:38 +0200

    2nd commit

--- app/src/main/java/vcmsa/projects/calculatorapp/Menu.kt
+++ app/src/main/java/vcmsa/projects/calculatorapp/Menu.kt
@@ -1,20 +1,28 @@
 package vcmsa.projects.calculatorapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonCalculator = findViewById<Button>(R.id.buttonCalculator)
+        val buttonNewFeatures = findViewById<Button>(R.id.buttonNewFeatures)
+
+        // Navigate to Calculator Screen
+        buttonCalculator.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Navigate to New Features Screen
+        buttonNewFeatures.setOnClickListener {
+            val intent = Intent(this, NewFeaturesActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/vcmsa/projects/calculatorapp/NewFeatures.kt
@@ -0,0 +1,11 @@
+package vcmsa.projects.calculatorapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewFeaturesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_features)
+    }
+}
